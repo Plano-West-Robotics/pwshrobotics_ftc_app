48073e6ac283e84c372d00a3692d3454e99dd681
akhilk999 <awesomesetty99@gmail.com>
2023-03-06 23:09:56 -0600

    added google fonts, created colors.dart for app theme, updated main.dart for app theme, added necessary dependencies in pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
+import './ui/colors.dart' as PWSHColors;
 
 void main() {
   runApp(const MyApp());
@@ -10,23 +12,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return DynamicTheme(
+      defaultBrightness: Brightness.light,
+      data: (brightness) => ThemeData(
+        primarySwatch: PWSHColors.Colors().pwshColors,
+        accentColor: brightness == Brightness.dark
+          ? PWSHColors.Colors().pwshColors.shade600
+            : null,
+        fontFamily: 'Google Sans',
+        cardTheme: CardTheme(
+          clipBehavior: Clip.antiAlias,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(6))
+          )
+        ),
+        brightness: brightness
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+      themedWidgetBuilder: (context, theme) {
+        return MaterialApp(
+          title: 'Plano West Robotics',
+          theme: theme,
+        );
+    }
+  );
 }
 
 class MyHomePage extends StatefulWidget {
