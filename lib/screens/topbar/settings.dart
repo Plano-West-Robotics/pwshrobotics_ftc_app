e8e97c63f3e76e06794f6e43aeae680b90c2291e
akhilk999 <awesomesetty99@gmail.com>
2023-03-07 22:04:11 -0600

    fixed settings dialog size

--- lib/screens/topbar/settings.dart
+++ lib/screens/topbar/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pwshrobotics_ftc_app/main.dart';
 
 class Settings extends StatefulWidget {
   @override
@@ -11,16 +12,18 @@ class _SettingsState extends State<Settings> {
     return IconButton(
         onPressed: () => showDialog<String>(
             context: context,
-            builder: (BuildContext context) => AlertDialog(
+            builder: (context) => AlertDialog(
               content: Column(
-                children: [
-                  TextButton(onPressed: () {},child: Text('Switch mode'),),
-                  TextButton(onPressed: () {},child: Text('About'),)
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextButton(onPressed: () {},child: const Text('Switch mode')),
+                  TextButton(onPressed: () {},child: const Text('About'))
                 ],
               )
  //             actions: <Widget>[TextButton(onPressed: () => Navigator.pop(context, 'Done'), child: const Text('Done'))],
             )
         ),
-        icon: Icon(Icons.settings));
+        icon: const Icon(Icons.settings));
   }
 }
