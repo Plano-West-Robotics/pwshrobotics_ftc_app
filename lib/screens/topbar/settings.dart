b50b2e7eb124ff8bc756be0866d08cf0b755c905
akhilk999 <awesomesetty99@gmail.com>
2023-03-08 01:22:56 -0600

    worked on alignment for settings modal bottom sheet

--- lib/screens/topbar/settings.dart
+++ lib/screens/topbar/settings.dart
@@ -6,26 +6,22 @@ class Settings extends StatefulWidget {
   _SettingsState createState() => _SettingsState();
 }
 
+
 class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return IconButton(
         onPressed: () {
-          showModalBottomSheet<void>(
+          showModalBottomSheet<dynamic>(
               context: context,
               builder: (BuildContext context) {
-                return SizedBox(
-                  height: 200,
-                  child: Center(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        TextButton(onPressed: () {},child: const Text('Switch mode')),
-                        TextButton(onPressed: () {},child: const Text('About'))
-                      ],
-                    )
-                  ),
+                return Wrap(
+                  //direction: Axis.vertical,
+                  children: <Widget>[
+                    Row(children: [Icon(Icons.notes),Expanded(child: TextButton(onPressed: () {},child:Text('Switch mode',textAlign: TextAlign.left)))],),
+                    TextButton.icon(onPressed: () {},icon: Icon(Icons.notes),label: const Text('About'))
+                  ],
+
                 );
               }
           );
@@ -33,6 +29,32 @@ class _SettingsState extends State<Settings> {
         icon: const Icon(Icons.settings));
   }
 }
+
+/*
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          showModalBottomSheet<dynamic>(
+              context: context,
+              builder: (BuildContext context) {
+                return Wrap(
+                  direction: Axis.vertical,
+                  children: <Widget>[
+                    TextButton.icon(onPressed: () {},icon: Icon(Icons.notes),label: const Text('Switch mode',textAlign: TextAlign.start,)),
+                    TextButton.icon(onPressed: () {},icon: Icon(Icons.notes),label: const Text('About'))
+                  ],
+
+                );
+              }
+          );
+        },
+        icon: const Icon(Icons.settings));
+  }
+}
+*/
+
 
 /*
 class _SettingsState extends State<Settings> {
