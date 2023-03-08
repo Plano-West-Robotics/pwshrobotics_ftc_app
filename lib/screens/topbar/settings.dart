839fe2779eec17cfbff60d461af273c16002a0fc
akhilk999 <awesomesetty99@gmail.com>
2023-03-07 22:45:37 -0600

    changed settings alert dialog into modal bottom sheet

--- lib/screens/topbar/settings.dart
+++ lib/screens/topbar/settings.dart
@@ -10,6 +10,35 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return IconButton(
+        onPressed: () {
+          showModalBottomSheet<void>(
+              context: context,
+              builder: (BuildContext context) {
+                return SizedBox(
+                  height: 200,
+                  child: Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        TextButton(onPressed: () {},child: const Text('Switch mode')),
+                        TextButton(onPressed: () {},child: const Text('About'))
+                      ],
+                    )
+                  ),
+                );
+              }
+          );
+        },
+        icon: const Icon(Icons.settings));
+  }
+}
+
+/*
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
         onPressed: () => showDialog<String>(
             context: context,
             builder: (context) => AlertDialog(
@@ -27,3 +56,4 @@ class _SettingsState extends State<Settings> {
         icon: const Icon(Icons.settings));
   }
 }
+ */
